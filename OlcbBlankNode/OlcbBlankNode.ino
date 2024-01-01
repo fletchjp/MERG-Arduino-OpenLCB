4abd1eecf58b235459429d5a65b25d285dd05a38
John Fletcher <john@bunbury28.plus.com>
2024-01-01 16:41:55 +0000

    Update OlcbBlankNode.ino
    Add a comment

--- OlcbBlankNode/OlcbBlankNode.ino
+++ OlcbBlankNode/OlcbBlankNode.ino
@@ -216,6 +216,7 @@ void produceFromInputs() {
       // This is needed to choose between CAN interfaces 0 or 1.
       // Note that it has to be done separately for Tx and Rx.
       // I am wondering if I can hide this DUE specific code from the user.
+      // I could do this in a constructor for the CAN interface.
       byte controllerInstance = 0;
       olcbcanTx.setControllerInstance(controllerInstance);
       olcbcanRx.setControllerInstance(controllerInstance);
